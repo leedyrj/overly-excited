a04e64f9a4f9ee6b112055decdbbfe70ea48d70a
Robert Leedy <leedyrj@gmail.com>
2018-05-23 12:54:56 -0500

    All challenges finished

--- overly-excited.js
+++ overly-excited.js
@@ -1,28 +1,16 @@
-// Create an array that contains the words in the sentence
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
-let nextSentence = ["The", "sealion", "jumped", "over", "the", "rocks", "in", "the", "light", "of", "the", "moon"];
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
-let addExcitement = (theWordArray, punctuation) => {
+let nextSentence = ["The", "sealion", "jumped", "over", "the", "rocks", "in", "the", "light", "of", "the", "moon"]
 
-    // Each time the for loop executes, you're going to add one more word to this string
-    let buildMeUp = ""
-
-    for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
-        buildMeUp += ` ${theWordArray[i]}`;
+let addExcitement = (theWordArray, punctuationMark) => {
+    let buildMeUp = "";
+    let char = punctuationMark
+       for (let i = 0; i < theWordArray.length; i++) {
+            buildMeUp += ` ${theWordArray[i]}`;      
         if ((i+1) % 3 === 0) {
-            buildMeUp += punctuation
+            buildMeUp += char;
+            char += punctuationMark;
         }
-        // Print buildMeUp to the console
-        //console.log(buildMeUp)
-        console.log(buildMeUp)
+          console.log(buildMeUp)
     }
-
 }
-
-// Invoke the function and pass in the array
-addExcitement(nextSentence, '&')
+addExcitement(nextSentence, '&');
